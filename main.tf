5f0404d348bb30036bd54bc6baa7f87e202b2966
vadim16795 <explosivedust@yandex.ru>
2021-08-21 17:34:56 +0300

    update main.tf, update variables.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,28 @@
+provider "google" {
+ credentials = var.google_creds
+ project     = "varkhipovgcloudpostgresql"
+ region      = "europe-west1"
+}
 resource "google_sql_database_instance" "master" {
-  name             = "master-instance"
-  database_version = "POSTGRES_11"
-  region           = "us-central1"
+  name             = "gcloudpostgresqlinstance"
+  database_version = "POSTGRES_9_6"
+  region           = "europe-west1"
 
   settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
     tier = "db-f1-micro"
+    availability_type = "ZONAL"
+    disk_autoresize = false
+    disk_size       = 10
+    disk_type       = "PD_HDD"
+      database_flags {
+        name = "cloudsql.iam_authentication"
+        value = "on"
+      }
   }
+}
+resource "google_sql_user" "users" {
+  name     = "postgres"
+  password = "changeme"
+  instance = google_sql_database_instance.master.name
+  type     = "CLOUD_IAM_USER"
 }
