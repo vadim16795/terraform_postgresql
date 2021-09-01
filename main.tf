5e89fff93bd927ac8f8e40006b5fab760ae5cb58
vadim16795 <explosivedust@yandex.ru>
2021-09-01 19:11:04 +0300

    update main.tf (add gke cluster)

--- main.tf
+++ main.tf
@@ -71,31 +71,25 @@ resource "postgresql_database" "stage" {
 #  display_name = "Service Account"
 #}
 
-#resource "google_container_cluster" "primary" {
-#  name     = "my-gke-cluster"
-#  location = "us-central1"
+resource "google_container_cluster" "primary" {
+  name     = "prod-app-cluster"
+  location = "europe-west1-c"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-#  remove_default_node_pool = true
-#  initial_node_count       = 1
-#}
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
 
-#resource "google_container_node_pool" "primary_preemptible_nodes" {
-#  name       = "my-node-pool"
-#  location   = "us-central1"
-#  cluster    = google_container_cluster.primary.name
-#  node_count = 1
-#
-#  node_config {
-#    preemptible  = true
-#    machine_type = "e2-medium"
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "prod-app-pool"
+  location   = "europe-west1-c"
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
 
-#    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-#    service_account = google_service_account.default.email
-#    oauth_scopes    = [
-#      "https://www.googleapis.com/auth/cloud-platform"
-#    ]
-#  }
-#}
+  node_config {
+    machine_type = "e2-medium"
+
+  }
+}
