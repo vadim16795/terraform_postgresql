a706843bedc3fe1b14e19749dad38b28b1d6d83c
vadim16795 <explosivedust@yandex.ru>
2021-09-01 18:02:04 +0300

    update main.tf (project changed)

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 
 provider "google" {
  credentials = var.google_creds
- project     = "varkhipovgcloudpostgresql"
+ project     = "varkhipovdiplomaproject"
  region      = "europe-west1"
 }
 resource "google_sql_database_instance" "master" {
@@ -69,3 +69,37 @@ resource "postgresql_database" "stage" {
   connection_limit  = -1
   allow_connections = true
 }
+
+#resource "google_service_account" "default" {
+#  account_id   = "service-account-id"
+#  display_name = "Service Account"
+#}
+
+#resource "google_container_cluster" "primary" {
+#  name     = "my-gke-cluster"
+#  location = "us-central1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+#  remove_default_node_pool = true
+#  initial_node_count       = 1
+#}
+
+#resource "google_container_node_pool" "primary_preemptible_nodes" {
+#  name       = "my-node-pool"
+#  location   = "us-central1"
+#  cluster    = google_container_cluster.primary.name
+#  node_count = 1
+#
+#  node_config {
+#    preemptible  = true
+#    machine_type = "e2-medium"
+
+#    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#    service_account = google_service_account.default.email
+#    oauth_scopes    = [
+#      "https://www.googleapis.com/auth/cloud-platform"
+#    ]
+#  }
+#}
