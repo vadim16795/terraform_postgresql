8d6303cc998b44d5126af94244da67b4a29e43e9
vadim16795 <explosivedust@yandex.ru>
2021-09-01 15:05:12 +0300

    update main.tf (add my_db)

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "google_sql_database_instance" "master" {
 }
 
 provider "postgresql" {
-  host            = tosting(google_sql_database_instance.master.ip_address)
+  host            = tostring(google_sql_database_instance.master.ip_address)
   port            = 5432
   username        = "postgres"
   password        = "Oper@t10n"
