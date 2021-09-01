69df6fc68f28467d2368ead5326c3fe5f9d3c384
vadim16795 <explosivedust@yandex.ru>
2021-09-01 15:39:49 +0300

    update main.tf (add new instance)

--- main.tf
+++ main.tf
@@ -40,6 +40,30 @@ resource "google_sql_database_instance" "master" {
   }
 }
 
+resource "google_sql_database_instance2" "master" {
+  name             = "gcloudpostgresqlinstance2"
+  database_version = "POSTGRES_9_6"
+  region           = "europe-west1"
+
+  settings {
+    tier = "db-f1-micro"
+    availability_type = "ZONAL"
+    disk_autoresize = false
+    disk_size       = 10
+    disk_type       = "PD_HDD"
+      ip_configuration {
+        ipv4_enabled = true
+        authorized_networks {
+          name= "all_networks"
+          value = "0.0.0.0/0"
+        }
+      }
+      database_flags {
+        name = "cloudsql.iam_authentication"
+        value = "on"
+      }
+  }
+}
 provider "postgresql" {
   host            = tostring(google_sql_database_instance.master.public_ip_address)
   port            = 5432
